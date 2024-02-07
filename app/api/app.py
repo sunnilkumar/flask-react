587bb161cfc8f19f44ac8e2e4f728631765a4ba8
Sunil Kumar <136375040+sunnilkumar@users.noreply.github.com>
2024-02-07 13:06:44 +0530

    Update app.py

--- app/api/app.py
+++ app/api/app.py
@@ -1,13 +1,14 @@
-from flask import Flask, request, jsonify, render_template,send_from_directory, Blueprint
-from flask_cors import CORS, cross_origin
+from flask import Flask, Blueprint, send_from_directory
 
-
-api = Blueprint('starter',__name__,static_folder='../react-app/build',static_url_path='')
+api = Blueprint('starter', __name__, static_folder='../react-app/build', static_url_path='')
 
 @api.route('/')
 def serve():
-    print("sdsdfs")
     return send_from_directory(api.static_folder, 'index.html')
-    
+
+
+
 if __name__ == '__main__':
-    api.run(host='0.0.0.0')
+    app = Flask(__name__)
+    app.register_blueprint(api)
+    app.run(host='0.0.0.0')
